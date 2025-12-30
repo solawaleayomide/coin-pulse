165c3d0fbaac1d159ac44fbf695841704494eb0f
solawaleayomide <olawalesamuel147@gmail.com>
2025-12-30 12:17:03 +0000

    implement navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function HomePage() {
-  return (
-    <main>
-      <h1 className="text-3xl text-indigo-500">Welcome to the Home Page</h1>
-    </main>
-  );
+  return <main></main>;
 }

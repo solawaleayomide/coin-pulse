bb84ce4d7127bbd95736511c587184a71053d8e7
solawaleayomide <olawalesamuel147@gmail.com>
2025-12-30 12:26:20 +0000

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 import { Header } from "@/components/header";
 
 const geistSans = Geist({

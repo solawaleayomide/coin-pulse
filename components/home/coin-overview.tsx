892e7f4758b9205e8af4ab63d92349401d57aebf
solawaleayomide <olawalesamuel147@gmail.com>
2026-01-03 13:50:42 +0000

    implement candlestickchart chart

--- components/home/coin-overview.tsx
+++ components/home/coin-overview.tsx
@@ -1,23 +1,49 @@
 import { fetcher } from "@/lib/coingecko.actions";
-import { formatCurrency } from "@/lib/utils";
+import { CoinOverviewFallback } from "../fallback";
 import Image from "next/image";
+import { formatCurrency } from "@/lib/utils";
+import { CandleStickChart } from "../candle-stick-chart";
 
 export async function CoinOverview() {
-  const coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-    dex_pair_format: "symbol",
-  });
+  let data: [CoinDetailsData, OHLCData] | null = null;
+
+  try {
+    data = await Promise.all([
+      fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+      fetcher<OHLCData>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: "1",
+        // interval: "hourly",
+        precision: "full",
+      }),
+    ]);
+  } catch (error) {
+    console.error("Error fetching coin data:", error);
+    return <CoinOverviewFallback />;
+  }
+
+  const [coin, coinOHLCData] = data;
+
   return (
     <div id="coin-overview">
-      <div className="header">
-        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-
-        <div className="info">
-          <p>
-            {coin.name} / {coin.symbol.toUpperCase()}
-          </p>
-          <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+      <CandleStickChart data={coinOHLCData} coinId="bitcoin">
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className="info">
+            <p>
+              {coin.name} / {coin.symbol.toUpperCase()}
+            </p>
+            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+          </div>
         </div>
-      </div>
+      </CandleStickChart>
     </div>
   );
 }
